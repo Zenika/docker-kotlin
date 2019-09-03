9e1efb0236ba5bf4f6acc5159eb72c36d607f21e
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-09-03 22:38:53 +0200

    :alembic:

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-export CIRCLE_BUILD_DATE=$(date -Ins --utc)
-
-docker image build --pull -t $TAG $BUILD_CONTEXT
+docker image build \
+  --pull \
+  -t $TAG \
+  --build-arg SOURCE=$SOURCE \
+  --build-arg CIRCLE_SHA1=$CIRCLE_SHA1 \
+  --build-arg TAG=$TAG \
+  --build-arg COMPILER_URL=$COMPILER_URL \
+  --build-arg CIRCLE_BUILD_DATE=$(date -Ins --utc) \
+  $BUILD_CONTEXT
